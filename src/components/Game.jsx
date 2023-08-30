fa6142b52802122a5c8c103604d8c8d0ebe0bd25
lubuntum <lubuntum@gmail.com>
2023-08-30 15:11:43 +0700

    Добавил возможность перемещаться по истории + исправил баг с выделением победных квадратов

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,11 +4,18 @@ export default function Game(){
     const [isNext, setIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [light, setLight] = useState(Array(9).fill("basic-light"))
-    const currentSquares = history[history.length-1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
     
     function handlePlay(nextSquares){
-        setHistory([...history, nextSquares]);
+        //Убераем прошлые ходы и обновляем историю
+        const nextHistory = [...history.slice(0, currentMove+1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length-1);
         setIsNext(!isNext);
+
+        //setHistory([...history, nextSquares]);
+        //setIsNext(!isNext);
     }
     
     function lightWinSquares(winSquares){
@@ -17,15 +24,21 @@ export default function Game(){
         setLight(nextLight);
     }
     function jumpTo(nextMove){
-        
+        setCurrentMove(nextMove);
+        setIsNext(nextMove % 2 === 0);
+        const nextLight = light.slice();
+        nextLight.forEach((value, i)=>{
+            if(value==="win-light") nextLight[i] = "basic-light";//ошибка
+        });
+        setLight(nextLight);
     }
 
     const moves = history.map((squares, move)=>{
         let desc;
         move > 0 ? desc = "Go to move #" + move: desc = "Go to start";
         return (
-            <li>
-                <buttom onClick={()=>jumpTo(move)}>{desc}</buttom>
+            <li key={move}>
+                <button onClick={()=>jumpTo(move)}>{desc}</button>
             </li>
         );
     });
